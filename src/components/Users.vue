aa24ad41214681850984771c42394275f475ec64
andersonyp <andersonyp@qq.com>
2018-12-21 22:00:06 +0800

    Users组件页面渲染完成

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,15 +1,147 @@
 <template>
   <div class="users">
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 搜索框 -->
+    <div style="margin-top: 15px;">
+      <el-input placeholder="请输入内容" v-model="query" class="input-with-select">
+        <el-button slot="append" icon="el-icon-search" @click="search"></el-button>
+      </el-input>
+      <el-button type="success" class="addBtn" plain>添加用户</el-button>
+    </div>
+    <!-- 数据渲染表格 -->
+    <el-table :data="userList" style="width: 100%">
+      <el-table-column prop="username" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="email" label="邮箱" width="180"></el-table-column>
+      <el-table-column prop="mobile" label="电话" width="180"></el-table-column>
+      <el-table-column prop="mg_state" label="状态" width="180">
+        <template slot-scope="scope">
+          <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button type="primary" icon="el-icon-edit" plain size="mini"></el-button>
+          <el-button
+            type="danger"
+            @click="delUser(scope.row.id)"
+            icon="el-icon-delete"
+            plain
+            size="mini"
+          ></el-button>
+          <el-button type="success" icon="el-icon-check" plain size="mini">分配角色</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 分页 -->
+    <el-pagination
+      background
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage"
+      :page-sizes="[2, 4, 6, 8]"
+      :page-size="pageSize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+    ></el-pagination>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
+  data() {
+    return {
+      userList: [],
+      query: '',
+      currentPage: 1,
+      pageSize: 2,
+      total: 0
+    }
+  },
+  methods: {
+    getUserList() {
+      axios({
+        method: 'get',
+        url: 'http://localhost:8888/api/private/v1/users',
+        params: {
+          query: this.query,
+          pagenum: this.currentPage,
+          pagesize: this.pageSize
+        },
+        headers: {
+          Authorization: localStorage.getItem('token')
+        }
+      }).then(res => {
+        if (res.data.meta.status === 200) {
+          this.userList = res.data.data.users
+          this.total = res.data.data.total
+        }
+      })
+    },
+    handleSizeChange(val) {
+      this.pageSize = val
+      this.getUserList()
+    },
+    handleCurrentChange(val) {
+      this.currentPage = val
+      this.getUserList()
+    },
+    search() {
+      this.currentPage = 1
+      this.getUserList()
+    },
+    delUser(id) {
+      this.$confirm('你确定要删除吗?', '温馨提示', {
+        type: 'wraning'
+      }).then(() => {
+        axios({
+          method: 'delete',
+          url: `http://localhost:8888/api/private/v1/users/${id}`,
+          headers: {
+            Authorization: localStorage.getItem('token')
+          }
+        }).then(res => {
+          if (res.data.meta.status === 200) {
+            if (this.userList.length <= 1 && this.currentPage > 1) {
+              this.currentPage--
+            }
+            this.$message.success('删除成功')
+            this.getUserList()
+          }
+        })
+      }).catch(() => {
+        this.$message.info('取消删除')
+      })
+    }
+  },
+  created() {
+    this.getUserList()
+  }
 }
 </script>
 
 <style lang="less" scoped>
-
+.el-breadcrumb {
+  height: 50px;
+  line-height: 50px;
+  background-color: #fff;
+  border-radius: 10px;
+  padding-left: 15px;
+}
+.el-input {
+  width: 350px;
+  margin-bottom: 15px;
+  border-radius: 10px;
+}
+.el-table {
+  border-radius: 10px;
+}
+.addBtn {
+  margin-left: 20px;
+}
 </style>
-
